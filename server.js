42184d45e23ea93bbf4d00a8009b3cef7d44acdd
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-10-13 00:16:22 +0300

    connecting API routes

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.envPORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'));
 
+app.use('/Api/users', require('./Routes/API/users'))
+app.use('/Api/auth', require('./Routes/API/auth'))
+app.use('/Api/posts', require('./Routes/API/posts'))
+app.use('/Api/profile', require('./Routes/API/profiles'))
+
 
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
